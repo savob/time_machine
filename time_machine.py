6b1f776952c731daa31ff35f5db8dce53bac0a27
Savo Bajic <bajic.savo@gmail.com>
2025-06-20 00:50:25 -0400

    Enabled scrolling image system

--- time_machine.py
+++ time_machine.py
@@ -62,7 +62,7 @@ def check_for_config_file(config_file, def_delay, def_photo_folder):
         result[0] = True # read file properly, all bits set
     return result
 
-# def run_photo_frame(params):
+def run_photo_frame(params):
 
     # grab our parameters that control operation of the
     configfn    = params[0]   # name of config file to look for in top level folder 
@@ -88,112 +88,81 @@ def check_for_config_file(config_file, def_delay, def_photo_folder):
         wid=1920.0
     #print(hgt,wid)
     
-    # scan the photoFolder for a list of picture files
+    # Scan the photo folder for a list of picture files
     pictureFiles=scan_for_files(photoFolder)
-    # Sort them by name (date)
     print(datetime.datetime.now(), 'Scan found', len(pictureFiles), 'files')
 
     # initialize for hourly and sleep processing    
-    done=False
+    done = False
+    last_hour = -1 # Force a reset on first iteration (for code testing)
+
+    # Force screen to remain on and not blank
+    if not IS_WINDOWS:
+        os.system("xset dpms force on")
+        os.system("xset s off")
     
     # and loop forever (until some key is hit)
     while not done:
-        
-        # during waking hours, display pictures
-        # during sleeping hours, keep display blanked
         for fn in pictureFiles:
 
-            # let's see if it's time to do hourly tasks
-            now=datetime.datetime.now()
-            hour=now.hour
-            if not IS_WINDOWS:
-                os.system("xset dpms force on")
+            # Hourly reconfiguration
+            current_hour = datetime.datetime.now().hour       
+            if current_hour is not last_hour:
+                last_hour = current_hour
 
-            #-- hourly tasks, only done when not sleeping            
-            if hour!=lastHour and not sleeping:
-                lastHour=hour
-                if not IS_WINDOWS:
-                    os.system("xset s off") # screen blanking off
-                # try to read configuration file instructions.ini
-                controlFn=os.path.join(photoFolder,configfn)
-                result=check_for_config_file(controlFn,delay,wakehour,bedtimehour,photoFolder)
-                if result[4]: # set to true for successful config file read
-                    delay=result[0]
-                    wakehour=result[1]
-                    bedtimehour=result[2]
-                    photoFolder=result[3]
-                # rescan folder
-                pictureFiles=scanForFiles(photoFolder)
-                random.shuffle(pictureFiles)
-                print(datetime.datetime.now(),'Scan found',len(pictureFiles),'files')
+                result = check_for_config_file(configfn, delay, photoFolder)
+                if result[0] == True:
+                    delay = result[1]
+                    photoFolder = result[2]
+                
+                pictureFiles = scan_for_files(photoFolder)
+                print(datetime.datetime.now(), 'Scan found', len(pictureFiles), 'files')
 
-            #--- run always, do wake up tasks or sleep tasks
-            #
-            # for example wakehour might be 9am and bedtimehour might be 9pm
-            # wakehour=9 and bedtimehour=21 (12+9)
-            if hour>=wakehour and hour<bedtimehour:
-                # we are in wake up time of day
+            #--- display a photo
+            gotImg=False
+            try:
+                print('loading', fn)
+                img = cv2.imread(fn, 1)
+                if len(img) > 0:
+                    gotImg = True
+            except:
+                gotImg = False
+            if not gotImg:
+                continue
 
-                #--- if we were sleeping, then it is time to wake up
-                if sleeping:
-                    print(datetime.datetime.now(),'Wake up')
-                    if not IS_WINDOWS:
-                        os.system("xset s off") # screen blanking off
-                        os.system("xset dpms force on")
-                    sleeping=False
+            #-- now, maybe resize image so it shows up well without changing the aspect ratio
+            #   add a border if the aspect ratio is different than the screen
+            # so we upscale or downscale so it maxes out either the
+            # horizontal or vertical portion of the screen
+            # then add a border around it to make sure any left-over
+            # parts of the screen are blacked out
+            widratio = wid / img.shape[1]
+            hgtratio = hgt / img.shape[0]
+            ratio = min(widratio,hgtratio)
+            dims = (int(ratio * img.shape[1]), int(ratio * img.shape[0]))
+            #print(fn, img.shape, ratio, dims[1], dims[0])
+            imgresized = cv2.resize(img, dims, interpolation = cv2.INTER_AREA)
+            #print(imgresized.shape)
+            # now, one dimension (width or height) will be same as screen dim
+            # and the other may be smaller than the screen dim.
+            # we're going to use cv.copyMakeBorder to add a border so we
+            # end up with an image that is exactly screen sized
+            widborder = max(1, int((wid-imgresized.shape[1]) / 2))
+            hgtborder = max(1, int((hgt-imgresized.shape[0]) / 2))
+            #print(hgtborder,widborder)
+            imgbordered = cv2.copyMakeBorder(imgresized, hgtborder, hgtborder, widborder, widborder, cv2.BORDER_CONSTANT)
+            #print('resized,bordered', imgbordered.shape)
 
-                #--- display a photo
-                # handle fault in loading a picture
-                gotImg=False
-                try:
-                    print('loading',fn)
-                    img = cv2.imread(fn, 1)
-                    if len(img)>0:
-                        gotImg=True
-                except:
-                    gotImg=False
-                if not gotImg:
-                    continue
-    
-                #-- now, maybe resize image so it shows up well without changing the aspect ratio
-                #   add a border if the aspect ratio is different than the screen
-                # so we upscale or downscale so it maxes out either the
-                # horizontal or vertical portion of the screen
-                # then add a border around it to make sure any left-over
-                # parts of the screen are blacked out
-                widratio=wid/img.shape[1]
-                hgtratio=hgt/img.shape[0]
-                ratio=min(widratio,hgtratio)
-                dims=(int(ratio*img.shape[1]),int(ratio*img.shape[0]))
-                #print(fn,img.shape,ratio,dims[1],dims[0])
-                imgresized=cv2.resize(img,dims,interpolation = cv2.INTER_AREA)
-                #print(imgresized.shape)
-                # now, one dimension (width or height) will be same as screen dim
-                # and the other may be smaller than the screen dim.
-                # we're going to use cv.copyMakeBorder to add a border so we
-                # end up with an image that is exactly screen sized
-                widborder=max(1,int((wid-imgresized.shape[1])/2))
-                hgtborder=max(1,int((hgt-imgresized.shape[0])/2))
-                #print(hgtborder,widborder)
-                imgbordered=cv2.copyMakeBorder(imgresized,hgtborder,hgtborder,widborder,widborder,cv2.BORDER_CONSTANT)
-                #print('resized,bordered',imgbordered.shape)
+            # and now show the image that has been resized and bordered
+            cv2.imshow(cv2frame, imgbordered)
+            #--- now we pause while the photo is displayed, we do this
+            #    by waiting for a key stroke.
+            k = cv2.waitKey(int(delay * 1000)) & 0xff
 
-                # and now show the image that has been resized and bordered
-                cv2.imshow(cv2frame, imgbordered)
-                #--- now we pause while the photo is displayed, we do this
-                #    by waiting for a key stroke.
-                k = cv2.waitKey(int(delay*1000)) & 0xff
-                # 255 if no key pressed (-1) or ascii-key-code (13=CR, 27=esc, 65=A, 32=spacebar)
-                if k!=0xff:
-                    # if a key was pressed, exit the photo frame program
-                    done=True
-                    break  
-            else:
-                sleeping=True
-                k = cv2.waitKey(300*1000) & 0xff # wait 300 seconds
-                # 255 if no key pressed (-1) or ascii-key-code (13=CR, 27=esc, 65=A, 32=spacebar)
-                if k!=0xff:
-                    done=True
+            if k != 255:
+                # if a key was pressed, exit the photo frame program
+                done = True
+                break  
   
     # when the photo display session ends, 
     # we need to clean up the cv2 full-frame window
@@ -232,4 +201,4 @@ if __name__ == "__main__":
     else:
         # and then, let's get this show on the road
         params=[config_file_name, delay_s, photo_folder]
-        # run_photo_frame(params)
+        run_photo_frame(params)
