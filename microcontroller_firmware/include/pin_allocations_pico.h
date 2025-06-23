cdfffeeae18dadf1d2eb8d6434ae85ad9b70661c
Savo Bajic <bajic.savo@gmail.com>
2025-06-23 00:39:39 -0400

    Updated pin allocations

--- microcontroller_firmware/include/pin_allocations_pico.h
+++ microcontroller_firmware/include/pin_allocations_pico.h
@@ -5,17 +5,18 @@
 
 #define PIN_LED		25		// For debugging purposes
 
-#define PIN_TOP_CLK 7
-#define PIN_TOP_DIO 8
+#define PIN_TOP_CLK 16
+#define PIN_TOP_DIO 17
 
-#define PIN_BOT_CLK 9
-#define PIN_BOT_DIO 10
+#define PIN_BOT_CLK 19
+#define PIN_BOT_DIO 20
 
-#define PIN_ENC_BUT 11		// Button pin for the encoder when pressed
-#define PIN_ENC_CLK 12		// "Clock" pin for the encoder, really just one phase
-#define PIN_ENC_DIR 13		// "Direction" pin for the encoder, really just one phase
+#define PIN_ENC_BUT 6		// Button pin for the encoder when pressed
+#define PIN_ENC_CLK 8		// "Clock" pin for the encoder, really just one phase
+#define PIN_ENC_DIR 7		// "Direction" pin for the encoder, really just one phase
 
-#define ACTIVE_ENC_BUT	LOW		// Active state of user button
-#define FORWARD_ENC_DIR	HIGH	// What state DIR needs to be to count as turning "forward"
+#define ACTIVE_ENC_BUT	LOW	// Active state of user button
+#define FORWARD_ENC_DIR	LOW	// What state DIR needs to be to count as turning "forward"
+
 
 #endif
