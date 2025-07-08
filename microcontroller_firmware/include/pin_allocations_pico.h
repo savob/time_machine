33bfdf148ba85d0022a32d43c48b010c08c75d85
Savo Bajic <bajic.savo@gmail.com>
2025-07-08 00:29:42 -0400

    Reflecting assembled system

--- microcontroller_firmware/include/pin_allocations_pico.h
+++ microcontroller_firmware/include/pin_allocations_pico.h
@@ -5,14 +5,14 @@
 
 #define PIN_LED		25		// For debugging purposes
 
-#define PIN_TOP_CLK 16
-#define PIN_TOP_DIO 17
+#define PIN_BOT_CLK 16
+#define PIN_BOT_DIO 17
 
-#define PIN_BOT_CLK 19
-#define PIN_BOT_DIO 20
+#define PIN_TOP_CLK 19
+#define PIN_TOP_DIO 20
 
 #define PIN_ENC_BUT 6		// Button pin for the encoder when pressed
-#define PIN_ENC_CLK 8		// "Clock" pin for the encoder, really just one phase
+#define PIN_ENC_CLK 9		// "Clock" pin for the encoder, really just one phase
 #define PIN_ENC_DIR 7		// "Direction" pin for the encoder, really just one phase
 
 #define ACTIVE_ENC_BUT	LOW	// Active state of user button
